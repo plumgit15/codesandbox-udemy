6a6e8130ea81bd9d8f612b55b275b5af30846250
plumgit15 <63837262+plumgit15@users.noreply.github.com>
2021-09-27 13:28:24 +0900

    test

--- src/index.js
+++ src/index.js
@@ -122,13 +122,74 @@
 // console.log(arr3);
 
 // 配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
 
-const arr6 = [...arr4];
-console.log(arr6);
+// const arr6 = [...arr4];
+// console.log(arr6);
 
-const arr7 = [...arr4, ...arr5];
-console.log(arr7);
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
 
 // イコールで配列をコピーすると参照が同じため、両方変わる。
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+// const nameArr = ["xx", "yy", "zz"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+//console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+
+// console.log(newNumArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件Trueの時：条件がfalseの時
+// const val1 = 1 > 0 ? "tureです" : "falseです";
+// console.log(val1);
+
+// const num = "1300";
+// // console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+/**
+ * 論理演算しの本当の意味を知ろう
+ */
+
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// \\ は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定";
+// console.log(fee);
+
+// // &&は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定された";
+// console.log(fee2);
